df5a309da5f6cfa7f7adc13a9167667bc6c4d59f
Kevin <k.kove2002@hotmail.dk>
2024-05-27 14:49:35 +0200

    Made process for CountSketch

--- CountSketch.cs
+++ CountSketch.cs
@@ -8,6 +8,23 @@ public class CountSketch {
         this.s = s;
         m = 1 << h.BitLen();
         this.sketch = new int[m];
+        for (int i = 0; i < m; i++){
+            sketch[i] = 0;
+        }
+    }
+
+    public ulong CalculateEstimate(){
+        ulong X = 0;
+        for (int y = 0; y < m; y++){
+            X += (ulong)(sketch[y]*sketch[y]);
+        }
+        return X;
+    }
+
+    public void Process(IEnumerable<(ulong, int)> stream){
+        foreach (var (x,d) in stream){
+            sketch[h(x)] += s(x)*d;
+        }
     }
 
 
